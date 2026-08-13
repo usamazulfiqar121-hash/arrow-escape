5eba2bc5050e10e414ddba74554cc4d46ef904dc
usamazu <your_email@gmail.com>
2026-08-13 11:51:28 +0500

    Add real AdMob ad unit IDs (Banner + Interstitial + Rewarded)

--- src/ads.js
+++ src/ads.js
@@ -1,9 +1,9 @@
 import { AdMob } from '@capacitor-community/admob';
 const TESTING = true;
 const TEST_IDS = {
-  banner: "ca-app-pub-3940256099942544/6300978111",
-  interstitial: "ca-app-pub-3940256099942544/1033173712",
-  rewarded: "ca-app-pub-3940256099942544/5224354917",
+  banner: "ca-app-pub-5743225482205913/5655183719",
+  interstitial: "ca-app-pub-5743225482205913/2019191036",
+  rewarded: "ca-app-pub-5743225482205913/9478878652",
 };
 const REAL_IDS = {
   banner: "YOUR_REAL_BANNER_ID",
