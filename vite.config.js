30ef25cefee4c39e27793aa1d6ec6e8687e77f53
Usama Ali <usamazulfiqar121@gmail.com>
2026-09-16 23:49:20 +0500

    Fix vite config syntax error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue' // (ya jo bhi framework aap use kar rahe ho)
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './', // <--- Yeh line zaroori add karein
+  base: './',
   plugins: [vue()],
-}
+})
