1dff7177ef0e9369e8e4e3c2e08747b24ae2b57b
Usama Ali <usamazulfiqar121@gmail.com>
2026-09-16 23:47:38 +0500

    Update vite config with base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue' // (ya jo bhi framework aap use kar rahe ho)
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './', // <--- Yeh line zaroori add karein
+  plugins: [vue()],
+}
